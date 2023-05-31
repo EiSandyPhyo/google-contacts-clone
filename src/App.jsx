5cdbfe46f95fb7e8ba3d38f45728536150e337b5
EiSandyPhyo <eisandyphyo345@gmail.com>
2023-05-31 22:10:29 +0630

    file setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactLists from './components/ContactLists'
+
+const App = () => {
+  return (
+    <div>
+      <ContactLists/>
+    </div>
+  )
+}
+
+export default App
